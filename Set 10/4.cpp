411ed443b5a4bc9b70e4f870ef2a3b97f9b99a79
sriv-PC <srivichai95@gmail.com>
2025-01-20 20:54:15 +0700

    Set 10

---
+++ Set 10/4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    int n1=(rand()%100)+1;
+    int n2=((rand()%13)+1)*2;
+    int n3=(rand()%11)-5;
+    int n4=pow(2,(rand()%10)+1);
+}
