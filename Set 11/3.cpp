7af72f3352577d1fd43175901d6882d1caf26b59
sriv95 <srivichai95@gmail.com>
2025-01-21 11:35:36 +0700

    Set 11

--- Set 11/3.cpp
+++ Set 11/3.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 long double func1(double N){
-    
+    if(N==1) return 2;
+    return pow(2,N)+func1(N-1);
+}
+
+long double func2(double N){
+    if(N==1) return 1;
+    return sqrt(N*func2(N-1));
+}
+
+long double func3(double N){
+    if(N==1) return 1;
+    return 1+(N/func3(N-1));
 }
 
 int main(){
-
+    cout<<func1(5)<<endl;
+    cout<<func2(5)<<endl;
+    cout<<func3(5)<<endl;
 }
